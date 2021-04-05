4dc82c895a423e8fc3f93bc5be0424929a338082
JACKCHAN000 <61930699+JACKCHAN000@users.noreply.github.com>
2021-04-05 22:54:39 +0800

    Update rime.lua

--- rime/rime.lua
+++ rime/rime.lua
@@ -1,3 +1,7 @@
-local google = require("trigger")("Control+t", require("google"))
-google_translator = google.translator
-google_processor = google.processor
+local c2e = require("c2etrigger")("Control+t", require("c2e"))
+c2e_translator = c2e.translator
+c2e_processor = c2e.processor
+
+local e2c = require("e2ctrigger")("Control+e", require("e2c"))
+e2c_translator = e2c.translator
+e2c_processor = e2c.processor
